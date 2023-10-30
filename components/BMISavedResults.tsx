53d6ea583e0b35d7cfc17c5f4f9b8f6b5a28b7d7
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-10-30 09:37:03 +0100

    Show bmi results

--- components/BMISavedResults.tsx
+++ components/BMISavedResults.tsx
@@ -1,9 +1,84 @@
-import { StyleSheet, Text } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useIsFocused } from "@react-navigation/native";
 
 export default function BMISavedResults() {
-  return <Text>Saved results</Text>;
+  const [results, setResults] = useState<any>([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    async function fetchSavedResults() {
+      try {
+        const savedResults = await AsyncStorage.getItem("bmiResults");
+        if (savedResults) {
+          setResults(JSON.parse(savedResults));
+        }
+      } catch (error) {
+        console.error("Error fetching saved results: ", error);
+      }
+    }
+
+    fetchSavedResults();
+  }, [isFocused]);
+
+  return (
+    <FlatList
+      data={results}
+      keyExtractor={(result, index) => index.toString()}
+      ListHeaderComponent={
+        <View style={styles.headerItem}>
+          <Text style={styles.headerText}>Data</Text>
+          <Text style={styles.headerText}>BMI</Text>
+        </View>
+      }
+      renderItem={({ item }) => (
+        <View style={styles.resultItem}>
+          <View style={styles.dateContainer}>
+            <Text style={styles.dateText}>{item.date}</Text>
+          </View>
+          <View style={styles.bmiContainer}>
+            <Text style={styles.bmiText}>{item.result}</Text>
+          </View>
+        </View>
+      )}
+    />
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 32,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  headerText: {
+    fontSize: 16,
+    padding: 12,
+    fontWeight: "bold",
+  },
+  resultItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  dateContainer: {
+    flex: 1,
+  },
+  bmiContainer: {
+    flex: 1,
+  },
+  dateText: {
+    fontSize: 16,
+  },
+  bmiText: {
+    fontSize: 16,
+    textAlign: "right",
+  },
+});
