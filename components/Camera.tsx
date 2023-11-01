d92206eb2eb07d8be68380ceb3316a519a9b106b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-01 13:43:41 +0100

    Primitive version of taking, saving and showing pictures

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -1,10 +1,123 @@
-import { StyleSheet, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
 
-export default function Camera() {
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { CameraCapturedPicture, Camera as CameraComponent } from "expo-camera";
+import { useEffect, useRef, useState } from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { shareAsync } from "expo-sharing";
+import { useIsFocused } from "@react-navigation/native";
+
+export default function App() {
+  let cameraRef = useRef<any>();
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] =
+    useState(false);
+  const [photo, setPhoto] = useState<CameraCapturedPicture | undefined>(
+    undefined
+  );
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission =
+        await CameraComponent.requestCameraPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+
+    if (!isFocused) {
+      setHasCameraPermission(false);
+      setHasMediaLibraryPermission(false);
+    }
+  }, [isFocused]);
+
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return (
+      <Text>
+        Permission for camera not granted. Please change this in settings.
+      </Text>
+    );
+  }
+
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+    };
+
+    let newPhoto = await cameraRef?.current?.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    let savePhoto = async () => {
+      if (photo) {
+        try {
+          // Create a directory to store photos if it doesn't exist
+          const directory = `${FileSystem.documentDirectory}photos/`;
+          await FileSystem.makeDirectoryAsync(directory, {
+            intermediates: true,
+          });
+
+          // Generate a unique file name for each saved photo
+          const fileName = `photo_${new Date().getTime()}.jpg`;
+          const fileUri = `${directory}${fileName}`;
+
+          // Save the photo to the local file system
+          await FileSystem.writeAsStringAsync(fileUri, photo.base64 || "", {
+            encoding: FileSystem.EncodingType.Base64,
+          });
+
+          // Clear the current photo
+          setPhoto(undefined);
+        } catch (error) {
+          console.error("Error saving photo:", error);
+        }
+      }
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.preview}
+          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+        />
+        <Button title="Share" onPress={sharePic} />
+        {hasMediaLibraryPermission ? (
+          <Button title="Save" onPress={savePhoto} />
+        ) : undefined}
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Camera</Text>
-    </View>
+    <CameraComponent style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button title="Take Pic" onPress={takePic} />
+      </View>
+      <StatusBar style="auto" />
+    </CameraComponent>
   );
 }
 
@@ -12,5 +125,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
+  },
+  preview: {
+    alignSelf: "stretch",
+    flex: 1,
   },
 });
--- components/PhotosList.tsx
+++ components/PhotosList.tsx
@@ -1,14 +1,51 @@
-import {
-StyleSheet,
-Text,
-View,
-} from "react-native";
+import * as FileSystem from "expo-file-system";
+
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+
+import { useIsFocused } from "@react-navigation/native";
 
 export default function PhotosList() {
+  const [savedPhotos, setSavedPhotos] = useState<any[]>([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    loadSavedPhotos();
+  }, [isFocused]);
+
+  const loadSavedPhotos = async () => {
+    try {
+      const directory = `${FileSystem.documentDirectory}photos/`;
+
+      // Read the contents of the photos directory
+      const photosDirectory = await FileSystem.readDirectoryAsync(directory);
+
+      // Create an array of image URIs
+      const imageUris = photosDirectory.map(fileName => ({
+        uri: `${directory}${fileName}`,
+      }));
+
+      setSavedPhotos(imageUris);
+    } catch (error) {
+      console.error("Error loading saved photos:", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>PhotosList</Text>
+      {savedPhotos.length > 0 && (
+        <FlatList
+          data={savedPhotos}
+          keyExtractor={item => item.uri}
+          renderItem={({ item }) => (
+            <View style={styles.imageContainer}>
+              <Image source={item} style={styles.image} />
+            </View>
+          )}
+        />
+      )}
+
+      {savedPhotos.length === 0 && <Text>No saved photos available.</Text>}
     </View>
   );
 }
@@ -17,5 +54,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    margin: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
