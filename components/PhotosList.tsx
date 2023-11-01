81d473c4dcb63cc232ffd5a286acdf9d5ba207e6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-01 19:58:05 +0100

    Add photo preview

--- components/PhotosList.tsx
+++ components/PhotosList.tsx
@@ -1,34 +1,44 @@
 import * as FileSystem from "expo-file-system";
 
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useEffect, useState } from "react";
-
-import { useIsFocused } from "@react-navigation/native";
 
 export default function PhotosList() {
   const [savedPhotos, setSavedPhotos] = useState<any[]>([]);
-  const isFocused = useIsFocused();
+  const [selectedPhotoUri, setSelectedPhotoUri] = useState<string | null>(null);
+  const isModalVisible = selectedPhotoUri !== null;
 
   useEffect(() => {
     loadSavedPhotos();
-  }, [isFocused]);
+  }, []);
 
   const loadSavedPhotos = async () => {
     try {
       const directory = `${FileSystem.documentDirectory}photos/`;
-
-      // Read the contents of the photos directory
       const photosDirectory = await FileSystem.readDirectoryAsync(directory);
-
-      // Create an array of image URIs
       const imageUris = photosDirectory.map(fileName => ({
         uri: `${directory}${fileName}`,
       }));
-
       setSavedPhotos(imageUris);
     } catch (error) {
       console.error("Error loading saved photos:", error);
     }
+  };
+
+  const openPhotoDetail = (photoUri: string) => {
+    setSelectedPhotoUri(photoUri);
+  };
+
+  const closePhotoDetail = () => {
+    setSelectedPhotoUri(null);
   };
 
   return (
@@ -40,12 +50,34 @@ export default function PhotosList() {
           keyExtractor={item => item.uri}
           numColumns={3}
           renderItem={({ item }) => (
-            <Image source={item} style={styles.image} />
+            <TouchableOpacity
+              style={styles.imageWrapper}
+              onPress={() => openPhotoDetail(item.uri)}
+            >
+              <Image source={item} style={styles.image} />
+            </TouchableOpacity>
           )}
         />
       )}
 
-      {savedPhotos.length === 0 && <Text>No saved photos available.</Text>}
+      {savedPhotos.length === 0 && <Text>Brak zdjęć</Text>}
+
+      <Modal
+        visible={isModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={closePhotoDetail}
+      >
+        <View style={styles.modalContainer}>
+          <Image
+            source={{ uri: selectedPhotoUri || "" }}
+            style={styles.modalImage}
+          />
+          <TouchableOpacity onPress={closePhotoDetail}>
+            <Text style={styles.closeButton}>Zamknij</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -59,9 +91,28 @@ const styles = StyleSheet.create({
   photosList: {
     width: "100%",
   },
-  image: {
+  imageWrapper: {
     width: "30%",
-    aspectRatio: 1,
     margin: 6,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  modalImage: {
+    width: "100%",
+    height: "90%",
+  },
+  closeButton: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 20,
   },
 });
