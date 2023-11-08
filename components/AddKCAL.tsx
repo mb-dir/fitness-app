d59524ec3a0315433397718bece93a74ebf7e408
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-08 21:47:49 +0100

    Routing inside kcal calculator

---
+++ components/AddKCAL.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from "react-native";
+
+import React from "react";
+
+export default function AddKCAL() {
+  return <Text>add kcal</Text>;
+}
+
+const styles = StyleSheet.create({});
---
+++ components/KCALSettings.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from "react-native";
+
+import React from "react";
+
+export default function KCALSettings() {
+  return <Text>KCAL settings</Text>;
+}
+
+const styles = StyleSheet.create({});
