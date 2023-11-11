dd3616dbdd6daf0bf50f3bf2520bedc542dc43cc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-11 03:35:13 +0100

    Rid of pedometr history

--- app/(tabs)/PedometrView.tsx
+++ app/(tabs)/PedometrView.tsx
@@ -1,5 +1,4 @@
 import Pedometr from "../../components/Pedometr";
-import PedometrHistory from "../../components/PedometrHistory";
 import PedometrSettings from "../../components/PedometrSettings";
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
@@ -9,7 +8,6 @@ export default function PedometrView() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Krokomierz" component={Pedometr} />
-      <Tab.Screen name="Historia" component={PedometrHistory} />
       <Tab.Screen name="Ustawienia" component={PedometrSettings} />
     </Tab.Navigator>
   );
