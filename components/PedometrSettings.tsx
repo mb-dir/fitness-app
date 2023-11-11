c0159f10989dda6414d5821c78bcbb49a3085f10
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-11 01:02:22 +0100

    User can set his step goal

--- components/PedometrSettings.tsx
+++ components/PedometrSettings.tsx
@@ -1,11 +1,80 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useEffect, useState } from "react";
 
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useIsFocused } from "@react-navigation/native";
 
 export default function PedometrSettings() {
+  const [stepGoal, setStepGoal] = useState("");
+  const [savedStepGoal, setSavedStepGoal] = useState<any>(null);
+  const isFocused = useIsFocused();
+
+  const saveStepGoal = async () => {
+    try {
+      await AsyncStorage.setItem("stepGoal", stepGoal.toString());
+      setSavedStepGoal(+stepGoal);
+      setStepGoal("");
+    } catch (error) {
+      console.error("Error saving step goal:", error);
+    }
+  };
+
+  const loadStepGoal = async () => {
+    try {
+      const goal = await AsyncStorage.getItem("stepGoal");
+      if (goal) {
+        setSavedStepGoal(parseInt(goal));
+      }
+    } catch (error) {
+      console.error("Error loading step goal:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadStepGoal();
+  }, [isFocused]);
+
   return (
-    <View>
-      <Text>PedometrSettings</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Ustal cel:</Text>
+      <TextInput
+        style={styles.input}
+        value={stepGoal}
+        onChangeText={text => setStepGoal(text)}
+        keyboardType="numeric"
+      />
+      <Button title="Save Step Goal" onPress={saveStepGoal} />
+      {savedStepGoal !== null && (
+        <View style={styles.savedGoalContainer}>
+          <Text style={styles.savedGoalText}>
+            Twój cel kroków: {savedStepGoal}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  savedGoalContainer: {
+    marginTop: 20,
+  },
+  savedGoalText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
