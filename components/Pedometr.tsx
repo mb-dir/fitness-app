eb4c45ad7ff650c87506802a4571c08b16e934c5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-11 03:17:33 +0100

    Logic for pedometr

--- components/Pedometr.tsx
+++ components/Pedometr.tsx
@@ -1,11 +1,105 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import React from "react";
+import { Accelerometer } from "expo-sensors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useIsFocused } from "@react-navigation/native";
 
-export default function Pedometr() {
+export default function StepCounter() {
+  const [stepCount, setStepCount] = useState(0);
+  const prevAccelerationRef = useRef(0);
+  const [isTracking, setIsTracking] = useState(true);
+  const [savedStepGoal, setSavedStepGoal] = useState(0);
+  const isFocused = useIsFocused();
+
+  const getStepGoal = async () => {
+    try {
+      const storedStepGoal = await AsyncStorage.getItem("stepGoal");
+      if (storedStepGoal !== null) {
+        setSavedStepGoal(+storedStepGoal);
+      }
+    } catch (error) {
+      console.error("Error getting step goal:", error);
+    }
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const subscribeToAccelerometer = async () => {
+      Accelerometer.setUpdateInterval(1000);
+
+      Accelerometer.addListener(data => {
+        if (isMounted && isTracking) {
+          detectSteps(data);
+        }
+      });
+    };
+
+    subscribeToAccelerometer();
+
+    return () => {
+      isMounted = false;
+      Accelerometer.removeAllListeners();
+    };
+  }, [isTracking]);
+
+  useEffect(() => {
+    getStepGoal();
+  }, [isFocused]);
+
+  const detectSteps = (data: { x: number; y: number; z: number }) => {
+    const { x, y, z } = data;
+    const currentAcceleration = Math.sqrt(x ** 2 + y ** 2 + z ** 2);
+    if (
+      prevAccelerationRef.current &&
+      Math.abs(currentAcceleration - prevAccelerationRef.current) >= 0.025
+    ) {
+      setStepCount(prevCount => prevCount + 1);
+    }
+
+    prevAccelerationRef.current = currentAcceleration;
+  };
+
+  const toggleTracking = () => {
+    setIsTracking(prevTracking => !prevTracking);
+  };
+
   return (
-    <View>
-      <Text>Pedometr</Text>
+    <View style={styles.container}>
+      <Text>
+        Kroki: {stepCount}/{savedStepGoal}
+      </Text>
+
+      <TouchableOpacity style={styles.toggleButton} onPress={toggleTracking}>
+        <Text style={styles.toggleButtonText}>
+          {isTracking ? "Wyłącz" : "włącz"}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  toggleButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#3498db",
+    borderRadius: 5,
+  },
+  toggleButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
